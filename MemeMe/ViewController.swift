7fcadbeac5dfba0754a613bb6f3e410db6247343
Xabier I. Losada <xi.losada@aditium.com>
2015-10-21 00:17:25 +0200

    add font selection and crop functionality

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate{
     
+    @IBOutlet weak var cropButtonItem: UIBarButtonItem!
     @IBOutlet weak var topToolbar: UIToolbar!
     @IBOutlet weak var frameView: UIView!
     @IBOutlet weak var bottomTextField: UITextField!
@@ -20,6 +21,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     var selectedImage: UIImage!
     var isKeyboardHidden: Bool = true
+    var isCroppModeEnabled: Bool = false
+    var fontName: String = "impact"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,18 +33,55 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         resetViews()
     }
 
+    @IBAction func cropImage(sender: AnyObject) {
+        self.imageView.contentMode = isCroppModeEnabled ? .ScaleAspectFit : .ScaleAspectFill
+        isCroppModeEnabled = !isCroppModeEnabled
+        self.cropButtonItem.title = isCroppModeEnabled ? "Fit " : "Crop"
+    }
+    
+    @IBAction func changeFont(sender: UIBarButtonItem) {
+        let controller = UIAlertController()
+        controller.title = "select the font"
+        controller.message = "anyFont"
+        let actionFontImpact = UIAlertAction(title:"Impact", style: UIAlertActionStyle.Default,handler:{
+            action in
+            self.fontName = "impact"
+            self.configureEditText(self.topTextField)
+            self.configureEditText(self.bottomTextField)
+            self.dismissViewControllerAnimated(true, completion:nil )
+        })
+        controller.addAction(actionFontImpact);
+        let actionFontOther = UIAlertAction(title:"Papyrus", style: UIAlertActionStyle.Default, handler: {
+            action in
+            self.fontName = "papyrus"
+            self.configureEditText(self.topTextField)
+            self.configureEditText(self.bottomTextField)
+            self.dismissViewControllerAnimated(true, completion:nil )
+        })
+        controller.addAction(actionFontOther)
+        let actionCancel = UIAlertAction(title:"Cancel", style: UIAlertActionStyle.Destructive, handler:{ action in
+            self.dismissViewControllerAnimated(true, completion:nil )
+            
+        })
+        controller.addAction(actionCancel)
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    
     func resetViews(){
         shareButtonItem.enabled = false
         topTextField.text = "TOP TEXT"
         bottomTextField.text = "BOTTOM TEXT"
         imageView.image = nil
+        cropButtonItem.title = "Fit"
+        cropButtonItem.enabled = false
     }
     
     func configureEditText(textField: UITextField){
         let memeDefaultText = [
             NSStrokeColorAttributeName : UIColor.blackColor(),
             NSForegroundColorAttributeName : UIColor.whiteColor(),
-            NSFontAttributeName : UIFont(name: "impact", size: 40)!,
+            NSFontAttributeName : UIFont(name: fontName, size: 40)!,
             NSStrokeWidthAttributeName : -3
             
         ]
@@ -119,6 +159,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.imageView.contentMode = .ScaleAspectFit
             self.imageView.image = pickedImage
             self.imageView.sizeThatFits(frameView.frame.size)
+            
+            
+            self.cropButtonItem.enabled = true
+            self.isCroppModeEnabled = false
             shareButtonItem.enabled = true
         }
         
@@ -131,6 +175,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     @IBAction func shareMeme(sender: UIBarButtonItem) {
         // Present the Activity View Controller
+        UIView.animateWithDuration(
+            NSTimeInterval(2.0),
+            animations: showToolBar,
+            completion: toolbarDidHide
+        )
+    }
+    
+    func showToolBar() {
+        topToolbar.frame.origin.y  -= topToolbar.frame.size.height;
+    }
+    
+    func hideToolBar() {
+        topToolbar.frame.origin.y  += topToolbar.frame.size.height;
+    }
+    
+    func toolbarDidHide( result: Bool){
+        createImage()
+    }
+    
+    func createImage(){
         selectedImage = generateMemedImage()
         let controller = UIActivityViewController(activityItems: [selectedImage], applicationActivities: nil)
         self.presentViewController(controller, animated: true, completion: save)
@@ -142,6 +206,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         print("BOT TEXT \(meme.bottomText)")
         print("BOT TEXT \(meme.image.description)")
         print("BOT TEXT \(meme.memedImage.description)")
+        UIView.animateWithDuration(
+            NSTimeInterval(2.0),
+            animations: hideToolBar,
+            completion: nil
+        )
     }
     
     func generateMemedImage() -> UIImage
@@ -150,7 +219,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
     UIGraphicsBeginImageContext(frameView.frame.size)
         let origin = CGPoint(x:0,y:0)
-        
         frameView.drawViewHierarchyInRect(CGRect(origin: origin, size: frameView.frame.size),afterScreenUpdates: true)
         let memedImage : UIImage =
         UIGraphicsGetImageFromCurrentImageContext()
